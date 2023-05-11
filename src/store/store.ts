de9c9da898fa8275570ada52fa13814ab8b522bf
Juan Pablo Gonzales <juampi7237@gmail.com>
2023-05-10 23:12:22 -0400

    fix store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,8 @@
 import { combineReducers, createStore } from "redux";
-//import { reducer } from "../reducer/reducer";
-import { initialState } from "../reducer/initial-state";
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "../slices/todoSlice";
 import { logger } from "./middleware/logger";
 import { todosApi } from "../api/todos-api";
-
-// let store = createStore(reducer, initialState);
 
 const rootReducer = combineReducers({
   todo: todoReducer,
@@ -15,8 +11,7 @@ const rootReducer = combineReducers({
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(todosApi.middleware).concat(logger),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(todosApi.middleware).concat(logger),
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
